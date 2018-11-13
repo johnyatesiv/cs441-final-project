01e9b411fb3d0bd93acbf230e80210a6b7922133
John Yates <john@locbit.com>
2018-11-12 16:53:50 -0800

    Reset port, remove proxy

--- app.js
+++ app.js
@@ -91,7 +91,6 @@ __delete = (route, req, res) => {
 
 };
 
-//const port = process.env.PORT || 5000;
-const port = 5000;
+const port = process.env.PORT || 5000;
 /** Starts the app, listening on 3000 by default or on the configured environment port **/
 app.listen(port, () => console.log('FoodApp up and running!'));
